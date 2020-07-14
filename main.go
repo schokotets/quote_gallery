10a362623cac3ba5a152d2477d46ea5fa3e6ca30
VXKFL <remo@stitronic.de>
2020-07-14 17:22:08 +0200

    Parse form

--- main.go
+++ main.go
@@ -14,8 +14,19 @@ func handlerMain(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerQuotes(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		if err := r.ParseForm(); err != nil {
+		    fmt.Fprintf(w, "ParseForm() err: %v", err)
+		    return
+		}
 
-	fmt.Fprintf(w, "Hi there, I love quotes!\n\n")
+		teacher := r.FormValue("teacher")
+		text := r.FormValue("text")
+		fmt.Fprintf(w, "teacher = %s\n", teacher)
+		fmt.Fprintf(w, "text = %s\n", text)
+	}
+
+	fmt.Fprintf(w, "\nHi there, I love quotes!\n\n")
 	fmt.Fprintf(w, strings.Join(quotes, "\n"))
 
 }
