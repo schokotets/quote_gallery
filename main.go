de2bf4c97e4524eb3d115bc98e7d3bcfc33cdd66
VXKFL <remo@stitronic.de>
2020-07-14 15:32:52 +0200

    Add handlerQuotes

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+var quotes = []string{"Dieser Junge hat keine Ahnung von go,", "aber das ist okay."}
+
+func handlerMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love chicken!", r.URL.Path[1:])
 }
+
+func handlerQuotes(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hi there, I love quotes!\n\n")
+	fmt.Fprintf(w, strings.Join(quotes, "\n"))
+}
+
+
+
+
 
 func main() {
 	log.Print("Starting website on :8080")
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", handlerMain)
+	http.HandleFunc("/quotes", handlerQuotes)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
