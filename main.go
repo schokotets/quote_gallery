440948ef0270dc8804ae72988426dcd45c06736a
Moritz Schulz <moritzleni@gmail.com>
2020-07-14 14:34:32 +0200

    initial webapp template

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	log.Print("Starting website on :8080")
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
