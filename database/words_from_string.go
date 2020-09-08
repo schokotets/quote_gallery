5d7ea9f68077cb3a12f27d58fe32c24ddcb9c88b
VXKFL <remo@stitronic.de>
2020-09-08 19:51:16 +0200

    Renamed words.go to words_from_string.go

--- database/words_from_string.go
+++ database/words_from_string.go
@@ -4,6 +4,8 @@ import "bytes"
 
 var characterLookup = [256]byte{}
 
+// sets up the characterLookup table
+// needs to be called before using getWordsFromString
 func setupCharacterLookup() {
 	for i := 0; i < 256; i++ {
 		if '0' <= i && i <= '9' {
@@ -38,7 +40,10 @@ func setupCharacterLookup() {
 	}
 }
 
-func words(s string) []string {
+// Processes a given string into a words
+// How the string is sliced is mainly dependend on the characterLookup which itself is created by setupCharacterLookup
+// setupCharacterLookup needs to have been called
+func getWordsFromString(s string) []string {
 	var buffer bytes.Buffer
 	buffer.Reset()
 	var words []string
