88d8ae2f19522830ee4c888193176d6b115dcc99
schokotets <moritzleni@gmail.com>
2020-11-29 12:00:53 +0100

    add edit and confirm operations to admin interface

---
+++ public/admin.js
@@ -0,0 +1,40 @@
+function http(method, url) {
+  axios({method: method, url: url})
+    .then(function (res) {
+      if(res.status == 200) {
+        window.location.reload()
+      } else {
+        return Promise.reject({response: res})
+      }
+    })
+    .catch(function (err) {
+      if("response" in err) { // if the error is axios-generated
+        alert("Fehler!\n"+axiosErrorString(err.response));
+      } else {
+        alert("Fehler!\n"+err.message);
+      }
+      console.error(err);
+    });
+
+  return true;
+}
+
+
+function axiosErrorString(response) {
+  if (!response) {
+    return "Keine Antwort erhalten";
+  }
+
+  let errorstr = "";
+  if (response.status) {
+    errorstr = "Status: " + response.status;
+  }
+  if (response.data) {
+    errorstr += "\nAntwort: " + response.data;
+  }
+  return errorstr;
+}
+
+let teacherselect = document.getElementById("teacherselect");
+let customteacher = document.getElementsByClassName("customteacher")[0];
+let customteacherfield = document.getElementById("customteacherfield");
