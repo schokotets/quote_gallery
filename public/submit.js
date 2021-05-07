236d5ae598009e82cd56766eb1ad6c30163a05d7
schokotets <schokotets@gmail.com>
2021-05-07 09:21:15 +0200

    format submit.js using prettier

--- public/submit.js
+++ public/submit.js
@@ -10,7 +10,9 @@ let customteachercheckbox = document.getElementById("certainthatcustom");
 
 let suggestionlist = document.getElementById("suggestionlist");
 let confirmdifferent = document.getElementById("confirmdifferent");
-let confirmdifferentcheckbox = document.getElementById("confirmdifferentcheckbox");
+let confirmdifferentcheckbox = document.getElementById(
+  "confirmdifferentcheckbox"
+);
 
 let submitbtn = document.getElementById("submitbtn");
 let clearformbtn = document.getElementById("clearform");
@@ -32,11 +34,11 @@ function processForm(e) {
   req["Text"] = quotefield.value;
 
   let context = contextfield.value;
-  if (context || EDITING){
+  if (context || EDITING) {
     req["Context"] = context;
   }
   let teacherid = teacherselect.value;
-  if (teacherid){
+  if (teacherid) {
     req["Teacher"] = parseInt(teacherid);
   } else {
     let teachername = customteacherfield.value;
@@ -47,14 +49,18 @@ function processForm(e) {
 
   let request;
   if (EDITING) {
-    request = axios.put("/api/unverifiedquotes/"+window.location.pathname.split("/")[3], req)
+    request = axios.put(
+      "/api/unverifiedquotes/" + window.location.pathname.split("/")[3],
+      req
+    );
   } else {
-    request = axios.post("/api/quotes/submit", req)
+    request = axios.post("/api/quotes/submit", req);
   }
-  request.then(function (res) {
-      if(res.status == 200) {
+  request
+    .then(function (res) {
+      if (res.status == 200) {
         clearForm();
-        if(EDITING) {
+        if (EDITING) {
           //hiding form because chrome re-shows last input values
           document.getElementById("form-submit").style.display = "none";
           window.location = document.referrer;
@@ -62,20 +68,21 @@ function processForm(e) {
           alert("Erfolgreich eingesendet!");
         }
       } else {
-        return Promise.reject({response: res});
+        return Promise.reject({ response: res });
       }
-    }).catch(function (err) {
-      if("response" in err) { // if the error is axios-generated
-        alert("Fehler beim Einsenden!\n"+axiosErrorString(err.response));
+    })
+    .catch(function (err) {
+      if ("response" in err) {
+        // if the error is axios-generated
+        alert("Fehler beim Einsenden!\n" + axiosErrorString(err.response));
       } else {
-        alert("Fehler beim Einsenden!\n"+err.message);
+        alert("Fehler beim Einsenden!\n" + err.message);
       }
       console.error(err);
     });
 
   return true;
 }
-
 
 function axiosErrorString(response) {
   if (!response) {
@@ -97,7 +104,8 @@ checkTeacherSelect();
 
 function checkTeacherSelect(e) {
   let target = e && e.target ? e.target : teacherselect;
-  if(target.selectedIndex == 1) { //custom field
+  if (target.selectedIndex == 1) {
+    //custom field
     customteacher.style.display = "unset";
     customteacherfield.setAttribute("required", true);
     customteachercheckbox.setAttribute("required", true);
@@ -151,7 +159,7 @@ customteachercheckbox.addEventListener("change", updateSubmitButtonState);
 
 function updateSubmitButtonState() {
   let allrequiredfilled = true;
-  for (let e of document.querySelectorAll('[required]')) {
+  for (let e of document.querySelectorAll("[required]")) {
     if (!e.value || (e.type == "checkbox" && !e.checked)) {
       allrequiredfilled = false;
       break;
@@ -165,13 +173,14 @@ function updateSubmitButtonState() {
 }
 
 function fetchSimilarQuotes() {
-  return axios.get('/suggestions', { params: { text: quotefield.value } } )
-    .then(function(res) {
+  return axios
+    .get("/suggestions", { params: { text: quotefield.value } })
+    .then(function (res) {
       if (quotefield.value != lastquotetext) {
         //while fetching, the text has changed again, request result irrelevant
         return;
       }
-      let similarQuotesHTML = res.data
+      let similarQuotesHTML = res.data;
       if (similarQuotesHTML) {
         suggestionlist.innerHTML = similarQuotesHTML;
         showConfirmDifferent();
